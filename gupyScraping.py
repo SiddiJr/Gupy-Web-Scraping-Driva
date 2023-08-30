fa4724cfb78de9c1ba5198d5b0a703408a72ec69
Sidnei Junior <sidnei.1998@alunos.utfpr.edu.br>
2023-08-30 09:32:51 -0300

    Atualização de nome de variável no script

--- gupyScraping.py
+++ gupyScraping.py
@@ -14,30 +14,30 @@ writer = csv.DictWriter(csvfile, fieldnames=header)
 writer.writeheader()
 
 # Cria o dicionário no qual as informações irão ser guardadas
-d = {"Nome": "", "Empresa": "", "Local": "", "Modelo": "", "Tipo": "", "Vaga para PcD": ""}
+dictVaga = {"Nome": "", "Empresa": "", "Local": "", "Modelo": "", "Tipo": "", "Vaga para PcD": ""}
 vagas = soup.find_all('a', class_="sc-a3bd7ea-1 kCVUJf")
 
 # Loop que itera pelas vagas
 for vaga in vagas:
     empresa = vaga.find('p', class_="sc-efBctP dpAAMR sc-a3bd7ea-6 cQyvth").text
     nomeVaga = vaga.find('h2', class_="sc-llJcti jgKUZ sc-a3bd7ea-5 XNNQK").text
-    d.update({"Nome": nomeVaga, "Empresa": empresa})
+    dictVaga.update({"Nome": nomeVaga, "Empresa": empresa})
     dados = vaga.find_all('div', class_="sc-23336bc7-2 hCmVmU")
     i = 0
 
     # Loop que itera pelas informações de local, tipo de emprego, modelo do emprego e se é vaga para PcD
     for dado in dados:
         if (i == 0):
-            d.update({"Local": dado.text})
+            dictVaga.update({"Local": dado.text})
         elif (i == 1):
-            d.update({"Tipo": dado.text})
+            dictVaga.update({"Tipo": dado.text})
         elif (i == 2):
-            d.update({"Modelo": dado.text})
+            dictVaga.update({"Modelo": dado.text})
         elif (i == 3):
-            d.update({"Vaga para PcD": "Sim"})
+            dictVaga.update({"Vaga para PcD": "Sim"})
         i += 1
-    if (d.get("Vaga para PcD") == ""):
-        d.update({"Vaga para PcD": "Não"})
+    if (dictVaga.get("Vaga para PcD") == ""):
+        dictVaga.update({"Vaga para PcD": "Não"})
 
     # Grava as informações no CSV
-    writer.writerow(d)
+    writer.writerow(dictVaga)
